40a1070e3bf06fca9c8731d5fce2a6318f56243e
mayyadah <miss.mayadah1@gmail.com>
2021-09-19 21:05:34 +0300

    not done

---
+++ src/components/list/list.js
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react';
+
+import { SettingsContext } from '../../context/settingContext';
+
+export default function List(props) {
+  const context = useContext(SettingsContext);
+
+
+
+
+}
+
